bc7954c481a0876c12e89a93dbc9ac154b33ff36
tetsu26 <moai26@outlook.com>
2018-06-15 00:36:01 +0900

    テストその１

--- servotest.py
+++ servotest.py
@@ -1,27 +1,12 @@
-# Simple demo of of the PCA9685 PWM servo/LED controller library.
-# This will move channel 0 from min to max position repeatedly.
-# Author: Tony DiCola
-# License: Public Domain
+
 from __future__ import division
 import time
 
 # Import the PCA9685 module.
 import Adafruit_PCA9685
 
-
-# Uncomment to enable debug output.
-#import logging
-#logging.basicConfig(level=logging.DEBUG)
-
 # Initialise the PCA9685 using the default address (0x40).
 pwm = Adafruit_PCA9685.PCA9685()
-
-# Alternatively specify a different address and/or bus:
-#pwm = Adafruit_PCA9685.PCA9685(address=0x41, busnum=2)
-
-# Configure min and max servo pulse lengths
-servo_min = 150  # Min pulse length out of 4096
-servo_max = 600  # Max pulse length out of 4096
 
 # Helper function to make setting a servo pulse width simpler.
 def set_servo_pulse(channel, pulse):
@@ -33,11 +18,16 @@ def set_servo_pulse(channel, pulse):
     pulse *= 1000
     pulse //= pulse_length
     pwm.set_pwm(channel, 0, pulse)
-
 # Set frequency to 60hz, good for servos.
 pwm.set_pwm_freq(60)
 
-print('Moving servo on channel 0, press Ctrl-C to quit...')
+print('please choose modes: 1:角度調整 2:動作試験')
+x=input()
+if x=1:
+    while True:
+        y=input("パルス幅を入力してください(700-2300):")
+        y=int(y)
+        set_servo_pulse(0,y)
 while True:
     # Move servo on channel O between extremes.
     pwm.set_pwm(0, 0, servo_min)
