0fd3acbe6c1ab3126f69809d5ef876bf89963d5a
tetsu26 <moai26@outlook.com>
2018-06-15 01:27:56 +0900

    iikanzi

--- servotest.py
+++ servotest.py
@@ -11,26 +11,32 @@ pwm = Adafruit_PCA9685.PCA9685()
 # Helper function to make setting a servo pulse width simpler.
 def set_servo_pulse(channel, pulse):
     pulse_length = 1000000    # 1,000,000 us per second
-    pulse_length //= 60       # 60 Hz
+    pulse_length //= 50       # 60 Hz
     print('{0}us per period'.format(pulse_length))
     pulse_length //= 4096     # 12 bits of resolution
     print('{0}us per bit'.format(pulse_length))
-    pulse *= 1000
+    #pulse *= 1000
     pulse //= pulse_length
     pwm.set_pwm(channel, 0, pulse)
 # Set frequency to 60hz, good for servos.
-pwm.set_pwm_freq(60)
+pwm.set_pwm_freq(50)
 
-print('please choose modes: 1:角度調整 2:動作試験')
+print('please choose modes: 1:Kakudo 2:EXAMINATION')
 x=input()
-if x=1:
+if x==1:
     while True:
-        y=input("パルス幅を入力してください(700-2300):")
+        y=input("pulse width(700-2300):")
         y=int(y)
         set_servo_pulse(0,y)
-while True:
-    # Move servo on channel O between extremes.
-    pwm.set_pwm(0, 0, servo_min)
-    time.sleep(1)
-    pwm.set_pwm(0, 0, servo_max)
-    time.sleep(1)
+if x==2:
+    y=input("initial pulse width(700-23000):")
+    y=int(y)
+    set_servo_pulse(0,y)
+    b=input("target pulse width(700-2300):")
+    b=int(b)
+    print("initial:",y,"target:",b)
+    z=input("If you are ready, press 'y':")
+    if z==y:
+        a=input("really?:")
+	if a==y:
+            set_servo_pulse(0,b)
