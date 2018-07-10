5f8ce1377cf3dcb749dbe07d6c934f9df9f292bb
tetsu26 <moai26@outlook.com>
2018-07-10 22:17:40 +0900

    aiueo

--- OpenCVtest/makemaker.cpp
+++ OpenCVtest/makemaker.cpp
@@ -3,22 +3,23 @@ using namespace std;
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/aruco.hpp>
+using namespace cv;
 
 int main(int argc, const char* argv[])
 {
     // dictionary生成
-    const cv::aruco::PREDEFINED_DICTIONARY_NAME dictionary_name = cv::aruco::DICT_4X4_50;
-    cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(dictionary_name);
+    const aruco::PREDEFINED_DICTIONARY_NAME dictionary_name = aruco::DICT_4X4_50;
+    Ptr<aruco::Dictionary> dictionary = aruco::getPredefinedDictionary(dictionary_name);
 
     // マーカー画像生成
     const int marker_id   = 0;
     const int side_pixels = 200;
-    cv::Mat marker_image;
-    cv::aruco::drawMarker(dictionary, marker_id, side_pixels, marker_image);
+    Mat marker_image;
+    aruco::drawMarker(dictionary, marker_id, side_pixels, marker_image);
 
     // 生成したマーカー画像を表示
-    cv::imshow("marker_image", marker_image);
-    cv::waitKey(0);
+    imshow("marker_image", marker_image);
+    waitKey(0);
 
     return 0;
 }
