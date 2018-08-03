89ae79a37b79d098491c3ff31ffa89f8ea63fa41
tetsu26 <moai26@outlook.com>
2018-08-03 22:36:02 +0900

    読み取りはできた気がする

--- OpenCVtest/read_aruco.cpp
+++ OpenCVtest/read_aruco.cpp
@@ -24,19 +24,25 @@ int main(int argc, const char* argv[])
 	height = InImage.rows;
 	
 	//カメラパラメータの読み込み
-	/*
+	
 	FileStorage fs2("out_camera_data.xml", FileStorage::READ);
     if (!fs2.isOpened()){
         cout << "File can not be opened." << endl;
         return -1;
     }
+     
     Mat cameraMatrix, distCoeffs;
+    /*
+    cameraMatrix = (Mat1f(3,3) << 1445.455931500428, 0, 960,0, 1445.455931500428, 540,0, 0, 1);
+    distCoeffs = (Mat1f(5,1) <<0.1433177732062716, -0.1956378847181435, 0, 0, -0.3822995443596148);
+    * */
+    
     fs2["camera_matrix"] >> cameraMatrix;
     fs2["distortion_coefficients"] >> distCoeffs;
 
     cout << "camera matrix: " << cameraMatrix<< endl
         << "distortion coeffs: " << distCoeffs<< endl;
-       */
+       
         
 	//入力画像に補正をかける とりあえず省略 
 	/*
@@ -47,12 +53,23 @@ int main(int argc, const char* argv[])
 	waitKey(0);
 	*/
 	vector<int> markerIds;
-	vector<std::vector<cv::Point2f>> markerCorners, rejectedCandidates;
-	Ptr<cv::aruco::DetectorParameters> parameters = cv::aruco::DetectorParameters::create();
+	vector<vector<Point2f>> markerCorners, rejectedCandidates;
+	Ptr<aruco::DetectorParameters> parameters = aruco::DetectorParameters::create();
 	aruco::detectMarkers(InImage, dictionary, markerCorners, markerIds,parameters,rejectedCandidates);
-	cvtColor(InImage, OutImage,COLOR_GRAY2RGB);
-	aruco::drawDetectedMarkers(OutImage, markerCorners, markerIds);
 	printf("%d\n",markerIds.size());
+	vector<Vec3d> rvecs, tvecs;
+	rvecs.clear();
+	tvecs.clear();
+	aruco::estimatePoseSingleMarkers(markerCorners, 0.05, cameraMatrix, distCoeffs, rvecs, tvecs);
+	printf("hoge\n");
+	for(auto a: rvecs){
+		cout << a<< endl;
+	}
+	for (int ii = 0; ii < markerIds.size(); ii++){
+		aruco::drawAxis(OutImage, cameraMatrix, distCoeffs, rvecs[ii], tvecs[ii], 0.1);
+	}
+
+	imwrite("out.jpg",OutImage);
 	imshow("out", OutImage);
 	waitKey(0); 
 	
